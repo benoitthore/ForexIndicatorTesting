04e4826d57582d88aa2d73747141731fd5c46eb4
Benoit Thore <thorebenoit@gmail.com>
2020-07-10 15:48:36 +0100

    testing

--- dll/src/nativeMain/kotlin/native.kt
+++ dll/src/nativeMain/kotlin/native.kt
@@ -1,6 +1,5 @@
 package example
 
-import com.bugsnag.example.kotlinmp.api.server.MT4WrapperImpl
 import com.bugsnag.example.kotlinmp.platformId
 import kotlinx.cinterop.CArrayPointer
 import kotlinx.cinterop.CPointer
@@ -8,17 +7,19 @@ import kotlinx.cinterop.DoubleVar
 import kotlinx.cinterop.IntVar
 
 
+var i = 0
+
 @CName(externName = "testFun", shortName = "testFun")
-fun createThing(): Int = platformId()
+fun createThing(): Int = i++
 
-val wrapper = MT4WrapperImpl()
-
-@CName(externName = "on_start", shortName = "on_start")
-fun onStart(action: CPointer<IntVar>, buffer: CArrayPointer<DoubleVar>, bufferSize: Int) {
-    wrapper.onStart(action.abstracted(), buffer.abstracted(bufferSize))
-}
-
-@CName(externName = "on_new_candle")
-fun onNewCandle() {
-    wrapper.onNewCandle()
-}
+//val wrapper = MT4WrapperImpl()
+//
+//@CName(externName = "on_start", shortName = "on_start")
+//fun onStart(action: CPointer<IntVar>, buffer: CArrayPointer<DoubleVar>, bufferSize: Int) {
+//    wrapper.onStart(action.abstracted(), buffer.abstracted(bufferSize))
+//}
+//
+//@CName(externName = "on_new_candle")
+//fun onNewCandle() {
+//    wrapper.onNewCandle()
+//}
